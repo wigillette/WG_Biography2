a931c7631fbacf921b910e68f6d4792d622763b9
Will Gillette <wgillette02@gmail.com>
2023-05-20 18:58:59 -0400

    Worked on header styling

--- js/init.js
+++ js/init.js
@@ -1,8 +1,9 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+(function ($) {
+  $(function () {
+    $('.sidenav').sidenav()
+    $('.parallax').parallax()
+    $('#header-container').load('./components/navbar.html')
+    $('#footer-container').load('./components/footer.html')
+  }) // end of document ready
+// eslint-disable-next-line no-undef
+})(jQuery) // end of jQuery name space
