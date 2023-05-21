fe478b8705655d45ded2b601809b4e338fe705f5
Will Gillette <wgillette02@gmail.com>
2023-05-21 10:19:18 -0400

    Created section placeholders

--- js/init.js
+++ js/init.js
@@ -4,6 +4,10 @@
     $('.parallax').parallax()
     $('#header-container').load('./components/navbar.html')
     $('#footer-container').load('./components/footer.html')
+    $('#overview-container').load('./components/overview.html')
+    $('#aspirations-container').load('./components/aspirations.html')
+    $('#portfolio-container').load('./components/portfolio.html')
+    $('#experiences-container').load('./components/experiences.html')
   }) // end of document ready
 // eslint-disable-next-line no-undef
 })(jQuery) // end of jQuery name space
